0b630ee71215471c55b89699a129d1ab600acf9c
khang <yourphotographysince2016@gmail.com>
2021-10-09 21:32:03 +0700

    first commit

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,28 @@
+/*
+Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as 
+forward. For example, 121 is palindrome while 123 is not.
+*/
+
+//No extra space
+
+//Algo: reverse only half of number and then
+//compare to avoid overflow
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+        {
+            return false;
+        }
+        int sum = 0;
+        while (x > sum)
+        {
+            sum = sum * 10 + x % 10;
+            x /= 10;
+        }
+        return (x == sum) || (x == sum / 10);
+    }
+};
