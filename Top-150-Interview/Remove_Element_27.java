b0597e47803c8403f0a814ba6e7ea576c6c1cf4a
Nick Nguyen <khangnick14@gmail.com>
2024-10-06 13:24:23 +0700

    solve problem easy 27

---
+++ Top-150-Interview/Remove_Element_27.java
@@ -0,0 +1,12 @@
+class Remove_Element_27 {
+  public int removeElement(int[] nums, int val) {
+    int res = 0;
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] != val) {
+        nums[res] = nums[i];
+        res++;
+      }
+    }
+    return res;
+  }
+}
